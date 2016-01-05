aef788c7f1e90a475a9e1ba13c8dd9415e7ee42c
Jeff Reiher <jreiher2003@yahoo.com>
2016-01-05 15:03:09 -0500

    add login.html, models.py, Procfile, blog.html and admin login part1

--- app.py
+++ app.py
@@ -1,8 +1,23 @@
 # import the Flask class from the flask module
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, \
+    url_for, request, session, flash
+from functools import wraps
+from flask.ext.sqlalchemy import SQLAlchemy
+import sqlite3
 
 # create the application object
 app = Flask(__name__)
+
+# config
+app.secret_key = 'my precious'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS '] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+
+# create the sqlalchemy object
+db = SQLAlchemy(app)
+
+# import db schema
+from models import *
 
 @app.route('/')
 @app.route('/home')
@@ -11,11 +26,34 @@ def index():
 
 @app.route('/blog')
 def blog():
-	return render_template('blog.html')
+	posts = db.session.query(BlogPost).all()
+	return render_template('blog.html', posts=posts)
 
 @app.route('/projects')
 def project():
 	return render_template('projects.html')
+
+# route for handling the login page logic
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if (request.form['username'] != 'admin') \
+                or request.form['password'] != 'admin':
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            session['logged_in'] = True
+            flash('You were logged in.')
+            return redirect(url_for('blog'))
+    return render_template('login.html', error=error)	
+
+
+@app.route('/logout')
+# @login_required
+def logout():
+    session.pop('logged_in', None)
+    flash('You were logged out.')
+    return redirect(url_for('welcome'))
 
 # start the server with the 'run()' method
 if __name__ == '__main__':
