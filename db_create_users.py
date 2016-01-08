4218a21d2633a67dcf5e8b67eecb7583f1efc433
Jeff Reiher <jreiher2003@yahoo.com>
2016-01-08 18:59:32 -0500

    add user script to generate 2 users

---
+++ db_create_users.py
@@ -0,0 +1,10 @@
+from app import db
+from models import User
+
+db.create_all()
+# insert data
+db.session.add(User("Jeffrey", "me@jeffreiher.com", "1234"))
+db.session.add(User("admin", "ad@min.com", "admin"))
+
+# commit the changes
+db.session.commit()
