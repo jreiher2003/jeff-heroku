edc9c722f7f4dd81c830d28307b1ef68ed6ae406
Jeffrey Reiher <jreiher2003@users.noreply.github.com>
2016-01-12 10:37:26 -0500

    Delete forms.py

--- project/forms.py
+++
@@ -1,9 +0,0 @@
-from flask_wtf import Form
-from wtforms import TextField, PasswordField 
-from wtforms.validators import DataRequired 
-
-class LoginForm(Form):
-	username = TextField('Username', validators=[DataRequired()])
-	password = PasswordField('Password', validators=[DataRequired()])
-
-
