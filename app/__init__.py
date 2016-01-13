423cd3946c5f0dddc930ed8192c20b84f1ed4d84
Jeff Reiher <jreiher2003@yahoo.com>
2016-01-13 15:54:02 -0500

    Add login-manager to project

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,29 @@
 from flask import Flask 
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.bcrypt import Bcrypt 
+from flask.ext.login import LoginManager 
+
+
 app = Flask(__name__)
+
+
 import os
 app.config.from_object(os.environ['APP_SETTINGS'])
 print os.environ['APP_SETTINGS']
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
 from app import views
 from models import *
+
+login_manager.login_view = "login"
+login_manager.login_message = u'You need to login to post'
+
+
+# loads users info from db and stores it in a session
+@login_manager.user_loader 
+def load_user(user_id):
+	return User.query.filter(User.id == int(user_id)).first()
