1d0176e2984825f6e8fed43426872766c9cb57c3
Jeff Reiher <jreiher2003@yahoo.com>
2016-01-13 15:55:40 -0500

    Create posting capabilites to blog
    made a form validator in forms.py
    update jinja2 code to beable to post only when logged in.
    update blog function in views.py to create and read from db.

--- app/forms.py
+++ app/forms.py
@@ -1,9 +1,12 @@
 from flask_wtf import Form
-from wtforms import TextField, PasswordField 
-from wtforms.validators import DataRequired 
+from wtforms import TextField, PasswordField, TextAreaField
+from wtforms.validators import DataRequired,Length
 
 class LoginForm(Form):
 	username = TextField('Username', validators=[DataRequired()])
 	password = PasswordField('Password', validators=[DataRequired()])
 
 
+class MessageForm(Form):
+	title = TextField('Title', validators=[DataRequired()])
+	description = TextAreaField('Description', validators=[DataRequired(),Length(max=140)])
--- app/views.py
+++ app/views.py
@@ -1,44 +1,57 @@
-from app import *
+from app import app, db
 from flask import render_template, redirect, \
     url_for, request, session, flash
-from forms import LoginForm
+from flask.ext.login import login_user, logout_user, login_required, current_user
+from forms import LoginForm, MessageForm
 
-from models import *
+from app.models import User, BlogPost, bcrypt
 
 @app.route('/')
 @app.route('/home')
 def index():
     return render_template('index.html')
 
-@app.route('/blog')
+@app.route('/blog', methods=["GET","POST"])
 def blog():
-	posts = db.session.query(BlogPost).all()
-	return render_template('blog.html', posts=posts)
+    error = None
+    form = MessageForm(request.form)
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            new_post = BlogPost(form.title.data,form.description.data,current_user.id)
+            db.session.add(new_post)
+            db.session.commit()
+            flash('New Post was successfully posted.', 'bg-success')
+            return redirect(url_for('blog'))
+    else:
+    	posts = db.session.query(BlogPost).all()
+    	return render_template('blog.html', posts=posts, form=form, error=error)
 
 @app.route('/projects')
 def project():
 	return render_template('projects.html')
 
 # route for handling the login page logic
-@app.route('/login', methods=['GET', 'POST'])
+@app.route('/login', methods=["GET", "POST"])
 def login():
-    mistake = None
+    error = None
     form = LoginForm(request.form)
-    if request.method == 'POST':
+    if request.method == "POST":
         if form.validate_on_submit():
             user = User.query.filter_by(name=request.form['username']).first()
             if user is not None and bcrypt.check_password_hash(user.password, request.form['password']):
-                session['logged_in'] = True
-                flash('You were logged in. Go Crazy')
-                return redirect(url_for('blog'))
-        else:
-            mistake = 'Invalid Credentials. Please try again.'
-    return render_template('login.html', mistake=mistake, form=form)	
+                login_user(user)
+                flash("You were logged in. Go Crazy.", 'bg-success')
+                return redirect(url_for('index'))
+            else:
+                flash("Try again", "bg-danger")
+                return redirect(url_for('login'))
+    return render_template("login.html", form=form, error=error)	
 
 
 @app.route('/logout')
 # @login_required
 def logout():
+    logout_user()
     session.pop('logged_in', None)
     flash('You were logged out.')
     return redirect(url_for('blog'))
