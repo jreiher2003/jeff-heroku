c258a5a240c675db789500e03f08f19ccbdb3bd0
Jeff Reiher <jreiher2003@yahoo.com>
2016-01-13 15:55:26 -0500

    Update post table __init__ include self.author_id = author_id

--- app/models.py
+++ app/models.py
@@ -2,21 +2,6 @@ from app import db, bcrypt
 from sqlalchemy import ForeignKey
 from sqlalchemy.orm import relationship
 
-class BlogPost(db.Model):
-
-    __tablename__ = "posts"
-
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String, nullable=False)
-    description = db.Column(db.String, nullable=False)
-    author_id = db.Column(db.Integer, ForeignKey('users.id'))
-
-    def __init__(self, title, description):
-        self.title = title
-        self.description = description
-
-    def __repr__(self):
-        return '<title> {}'.format(self.title)
 
 
 class User(db.Model):
@@ -34,5 +19,35 @@ class User(db.Model):
         self.email = email
         self.password = bcrypt.generate_password_hash(password)
 
+    def is_authenticated(self):
+        return True
+
+    def is_active(self):
+        return True
+
+    def is_anonymous(self):
+        return False
+
+    def get_id(self):
+        return unicode(self.id)
+
     def __repr__(self):
         return '<name> {}'.format(self.name)
+
+
+class BlogPost(db.Model):
+
+    __tablename__ = "posts"
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, nullable=False)
+    description = db.Column(db.String, nullable=False)
+    author_id = db.Column(db.Integer, ForeignKey('users.id'))
+
+    def __init__(self, title, description, author_id):
+        self.title = title
+        self.description = description
+        self.author_id = author_id
+
+    def __repr__(self):
+        return '<title> {}'.format(self.title)
