15ac2d4cf40a7a0005847c95f124d249b65738aa
Jeff Reiher <jreiher2003@yahoo.com>
2016-01-18 10:55:25 -0500

    Update styles to login form
    refactor views.py to not check post, changed styles on flashes and base1.html.

--- app/views.py
+++ app/views.py
@@ -47,16 +47,16 @@ def deleteBlogPost(author_id, blog_id):
 def login():
     error = None
     form = LoginForm(request.form)
-    if request.method == "POST":
-        if form.validate_on_submit():
-            user = User.query.filter_by(name=request.form['username']).first()
-            if user is not None and bcrypt.check_password_hash(user.password, request.form['password']):
-                login_user(user)
-                flash("You were logged in. Go Crazy.", 'bg-success')
-                return redirect(url_for('index'))
-            else:
-                flash("Try again", "bg-danger")
-                return redirect(url_for('login'))
+    # if request.method == "POST":
+    if form.validate_on_submit():
+        user = User.query.filter_by(name=request.form['username']).first()
+        if user is not None and bcrypt.check_password_hash(user.password, request.form['password']):
+            login_user(user)
+            flash("You were logged in. Go Crazy.", 'bg-success')
+            return redirect(url_for('index'))
+        else:
+            flash("Try again", "bg-danger")
+            return redirect(url_for('login'))
     return render_template("login.html", form=form, error=error)	
 
 
@@ -65,6 +65,6 @@ def login():
 def logout():
     logout_user()
     session.pop('logged_in', None)
-    flash('You were logged out.')
+    flash('You were logged out.', 'bg-warning')
     return redirect(url_for('blog'))
 
