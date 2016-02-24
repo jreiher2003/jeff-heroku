5a94d9dc36b94866c0471228a26ca47cfc12f791
Jeff Reiher <jreiher2003@yahoo.com>
2016-02-24 16:29:01 -0500

    Config test suite with coverage

--- config.py
+++ config.py
@@ -1,19 +1,23 @@
 import os
-import secret_key
 
-# default config
 class BaseConfig(object):
     DEBUG = False
-    SECRET_KEY = secret_key.secret_key
     SQLALCHEMY_TRACK_MODIFICATIONS = True
+    SECRET_KEY = os.environ['SECRET_KEY']
     SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
-    POSTS_PER_PAGE = 10
-    print SQLALCHEMY_DATABASE_URI
-   
+
+
+class TestConfig(BaseConfig):
+    DEBUG = True
+    TESTING = True
+    MAIL_SUPPRESS_SEND = False
+    WTF_CSRF_ENABLED = False
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'
 
 
 class DevelopmentConfig(BaseConfig):
     DEBUG = True
+    # print SQLALCHEMY_DATABASE_URI
 
 
 class ProductionConfig(BaseConfig):
